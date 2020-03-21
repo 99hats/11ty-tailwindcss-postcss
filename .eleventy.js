e0a87d02b459ee28023d11e61be049eca8a1e94f
Kevin Smith <kevin@mcweekly.com>
2020-03-21 15:55:25 -0700

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig) {
   
   eleventyConfig.setDataDeepMerge(true);
-  
+  // eleventyConfig.addPassthroughCopy(true);
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+
   return {
     dir: {
       input: "src",
